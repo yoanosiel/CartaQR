b721311ccea958beecc1a7d53b636c14f8c7600e
yoanosiel <151474332+yoanosiel@users.noreply.github.com>
2026-08-29 10:19:46 -0400

    Create CrashCatcherProvider.kt

---
+++ app/src/main/java/com/example/CrashCatcherProvider.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class CrashCatcherProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        context?.let { CrashCatcher.install(it) }
+        return true
+    }
+
+    override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? = null
+    override fun getType(uri: Uri): String? = null
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int = 0
+}
